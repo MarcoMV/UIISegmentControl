025f9ea7d65748226fc0ad9da57676cd3d92fb4e
MarcoMV <markovera93@gmail.com>
2015-06-05 16:07:22 -0500

    Usando el segmented control para cambiar de imágenes

--- UIISegmentControl/ViewController.swift
+++ UIISegmentControl/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var miSegment: UISegmentedControl!
 
+    @IBOutlet weak var miImagen: UIImageView!
+    @IBOutlet weak var miLabel: UILabel!
+    @IBAction func procesar(sender: AnyObject) {
+        
+        if miSegment.selectedSegmentIndex == 0 {
+            
+            println("Uno")
+            miImagen.image = UIImage (named: "app.jpg")
+            miLabel.text = "IOS"
+        } else if miSegment.selectedSegmentIndex == 1 {
+            println("Dos")
+            miImagen.image = UIImage (named: "wal.jpg")
+            miLabel.text = "Loco"
+        } else
+        {
+            println("tres")
+            miImagen.image = UIImage (named: "tres.jpg")
+            miLabel.text = "Mountains"
+            
+        }
+        
+        
+    
+        
+        
+    }
+    @IBOutlet weak var miSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+           miLabel.text = "IOS"
     }
 
     override func didReceiveMemoryWarning() {
